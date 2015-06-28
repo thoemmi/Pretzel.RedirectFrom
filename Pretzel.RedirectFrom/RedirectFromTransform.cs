20d4821290937272ca32e843e6699a8a3d4dc0af
Thomas Freudenberg <info@thomasfreudenberg.com>
2015-06-28 18:48:03 +0200

    initial checkin

---
+++ Pretzel.RedirectFrom/RedirectFromTransform.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO.Abstractions;
+using System.Linq;
+using Pretzel.Logic.Extensibility;
+using Pretzel.Logic.Templating.Context;
+using Pretzel.RedirectFrom.Properties;
+
+namespace Pretzel.RedirectFrom
+{
+    public class RedirectFromTransform : ITransform
+    {
+        private readonly IFileSystem _fileSystem;
+
+        [ImportingConstructor]
+        public RedirectFromTransform(IFileSystem fileSystem) {
+            _fileSystem = fileSystem;
+        }
+
+        public void Transform(SiteContext siteContext) {
+            foreach (var post in siteContext.Posts.Concat(siteContext.Pages).Where(p => !(p is NonProcessedPage))) {
+                object obj;
+                if (post.Bag.TryGetValue("redirect_from", out obj)) {
+                    var sourceUrls = obj as IEnumerable<string>;
+                    if (sourceUrls != null) {
+                        WriteRedirectFile(siteContext, post, sourceUrls);
+                    }
+                }
+            }
+        }
+
+        private void WriteRedirectFile(SiteContext siteContext, Page post, IEnumerable<string> sourceUrls) {
+            var targetUrl = post.Url;
+            var content = String.Format(Templates.Redirect, targetUrl);
+
+            foreach (var sourceUrl in sourceUrls) {
+                var directory = _fileSystem.Path.Combine(siteContext.OutputFolder, sourceUrl.TrimStart('/').Replace('/', '\\'));
+                _fileSystem.Directory.CreateDirectory(directory);
+                _fileSystem.File.WriteAllText(_fileSystem.Path.Combine(directory, "index.html"), content);
+            }
+        }
+    }
+}
