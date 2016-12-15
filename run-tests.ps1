b54877e5487e3a7ab7f3052eb5c6da73a017abdd
Thomas Freudenberg <info@thomasfreudenberg.com>
2016-12-15 21:46:29 +0100

    collection test

--- run-tests.ps1
+++ run-tests.ps1
@@ -12,7 +12,6 @@ if (!(Test-Path $testRoot)) {
     New-Item $testRoot -ItemType Directory | Out-Null
 }
 
-
 #----------------------------------------
 
 Write-Host "Testing scalar redirect" -ForegroundColor Yellow
@@ -44,6 +43,43 @@ $expectedContent = @"
 </script>
 "@ -split "`r`n" | ? { $_ -ne "" }
 Assert-FileContent -Filename "$testSite\_site\pages\page1\index.html" -ExpectedContent $expectedContent
+
+Remove-Item $testSite -Confirm:$false -Force -Recurse
+
+#----------------------------------------
+
+Write-Host "Testing list redirect" -ForegroundColor Yellow
+
+$testSite = New-TestSite $testRoot
+Set-Content (Join-Path $testSite "_posts\2016-10-31-myfirstpost.md") @"
+---
+layout: post
+title: "My First Post"
+author: "Author"
+comments: true
+redirect_from:
+  - /pages/page2
+  - /pages/page3.aspx
+---
+test
+"@
+
+Bake-Pretzel -PretzelExe $pretzelExe -Site $testSite
+
+$expectedContent = @"
+<!DOCTYPE html>
+<meta charset="utf-8" />
+<title>Redirecting...</title>
+<link rel="canonical" href="/2016/10/31/myfirstpost.html" />
+<meta http-equiv="refresh" content="0; url=/2016/10/31/myfirstpost.html" />
+<h1>Redirecting...</h1>
+<a href="/2016/10/31/myfirstpost.html">Click here if you are not redirected.</a>
+<script>
+    location="/2016/10/31/myfirstpost.html"
+</script>
+"@ -split "`r`n" | ? { $_ -ne "" }
+Assert-FileContent -Filename "$testSite\_site\pages\page2\index.html" -ExpectedContent $expectedContent
+Assert-FileContent -Filename "$testSite\_site\pages\page3.aspx\index.html" -ExpectedContent $expectedContent
 
 Remove-Item $testSite -Confirm:$false -Force -Recurse
 
